ddd42afe662efffc0740d9d086881bdc9d7a4ef4
Tanishq Dubey <tanishq.dubey@gmail.com>
2022-12-21 12:07:37 -0600

    Tons of new code, can now generate codes. Need to add refreshing

--- Sesame/ContentView.swift
+++ Sesame/ContentView.swift
@@ -7,11 +7,23 @@
 
 import SwiftUI
 import CoreData
+import CodeScanner
+
+struct AddSheetView: View {
+    @Environment(\.dismiss) var dismiss
+    
+    var body: some View {
+        Button("dismiss") {
+            dismiss()
+        }
+    }
+}
 
 struct ContentView: View {
-    @State var otpList = [
-        try! OTPItem("otpauth://totp/DWS%20LLC.:admin@dws.rip?secret=SESAMETEST&issuer=DWS%20LLC.&algorithm=SHA1&digits=6&period=30"),
-        try! OTPItem("otpauth://hotp/DWS%20LLC.:admin@dws.rip?secret=SESAMETEST&algorithm=SHA1&digits=6&period=30")
+    @State private var showingAdd = false
+    
+    @State var otpList: [OTPItem] = [
+        try! OTPItem("otpauth://totp/ACME%20Co:john@example.com?secret=HXDMVJECJJWSRB3HWIZR4IFUGFTMXBOZ&issuer=ACME%20Co&algorithm=SHA1&digits=6&period=30")
     ]
     
     var body: some View {
@@ -26,23 +38,51 @@ struct ContentView: View {
                     }
                     ToolbarItem(placement: .navigationBarTrailing) {
                         Button("Add New") {
-                            print("hello")
+                            showingAdd = true
                         }
                     }
+                }
+                .sheet(isPresented: $showingAdd) {
+                    VStack{
+                        CodeScannerView(codeTypes: [.qr], simulatedData: "otpauth://hotp/DWS%20LLC.:admin@dws.rip?secret=SESAMETEST&algorithm=SHA1&digits=6&period=30", completion: handleScan)
+                        Text("testing")
+                    }
+                    
                 }
         }
     }
     
-    func addOTP() {
-        
+    func handleScan(result: Result<ScanResult, ScanError>) {
+        showingAdd = false
+        switch result {
+        case .success(let result):
+            let details = result.string
+            print(details)
+            do {
+                let item = try OTPItem(details)
+                print(item.generateCode())
+                otpList.append(item)
+            } catch OTPError.malformedInput {
+                print("malformed input: \(details)")
+            } catch OTPError.parsingError {
+                print("parsing error: \(details)")
+            } catch {
+                print("who knows what")
+            }
+            
+        case .failure(let error):
+            print(error.localizedDescription)
+        }
     }
+    
 }
 
 struct TOTPRowView: View {
     var TOTPItem: OTPItem
     var body: some View {
         VStack{
-            Text(TOTPItem.secret)
+            Text(TOTPItem.currentValue)
+                .font(.title)
             HStack{
                 Text(TOTPItem.issuer)
                     .font(.subheadline)
--- Sesame/OTPModel.swift
+++ Sesame/OTPModel.swift
@@ -6,6 +6,9 @@
 //
 
 import Foundation
+import Base32
+import CryptoKit
+import CommonCrypto
 
 enum OTPAlgorithm {
     case SHA1
@@ -31,46 +34,88 @@ enum OTPError: Error {
 }
 
 class OTPItem: ObservableObject, Identifiable {
-    // A OTPItem represents a single OTP (TOTP or HOTP) key.
-    // This item is generated via QR code or URL in the following format:
-    //
+    /// A OTPItem represents a single OTP (TOTP or HOTP) key.
+    /// This item is generated via QR code or URL in the following format:
+    
     // otpauth://totp/DWS%20LLC.:admin@dws.rip?secret=SESAMETEST&issuer=DWS%20LLC.&algorithm=SHA1&digits=6&period=30
     //
-    // Here we have the otpauth URL scheme indicating it is for OTP:
-    //  - The totp portion differentiates HOTP or TOTP (HMAC or Time)
-    //  - The DWS%20LLC.:admin@dws.rip is the label. The issuer can be inferred from this
-    //  - The secret is the secret key (required)
-    //  - The issuer is the issuer (optional)
-    //  - The algorithm is the hashing to use in HMAC (SHA1*, SHA256, SHA512) (optional)
-    //  - The digits is how big of a code to generate (6*, 8) (optional)
-    //  - The period (only valid on TOTP) is the time interval (30*) (optional)
-    //  - The counter (only valid on HOTP) is the initial sync counter (required)
+    /// Here we have the otpauth URL scheme indicating it is for OTP:
+    ///  - The totp portion differentiates HOTP or TOTP (HMAC or Time)
+    ///  - The DWS%20LLC.:admin@dws.rip is the label. The issuer can be inferred from this
+    ///  - The secret is the secret key (required)
+    ///  - The issuer is the issuer (optional)
+    ///  - The algorithm is the hashing to use in HMAC (SHA1*, SHA256, SHA512) (optional)
+    ///  - The digits is how big of a code to generate (6*, 8) (optional)
+    ///  - The period (only valid on TOTP) is the time interval (30*) (optional)
+    ///  - The counter (only valid on HOTP) is the initial sync counter (required)
 
-    // Generic UUID for unique identification in CoreData
+    /// Generic UUID for unique identification in CoreData
     let id = UUID()
     
-    // The type is either HOTP or TOTP. We default to TOTP since it is the most common
+    /// The type is either HOTP or TOTP. We default to TOTP since it is the most common
     var type: OTPType = OTPType.TOTP
     
-    // The secret is a Base32 encoded string of the secret key shared between
-    // client and user.
+    /// The secret is a Base32 encoded string of the secret key shared between client and user.
     var secret: String
     
-    // The issuer is a string indicating the provider or service the key is associated with. It is URL-encoded.
-    // If this parameter is not available, the issuer can be taken from the prefix on the label.
+    /// The issuer is a string indicating the provider or service the key is associated with. It is URL-encoded.
+    /// If this parameter is not available, the issuer can be taken from the prefix on the label.
     @Published var issuer: String
     
-    // The algorithm to use when hashing in HMAC. SHA1 is the most common.
+    /// The algorithm to use when hashing in HMAC. SHA1 is the most common.
     var algorithm: OTPAlgorithm = OTPAlgorithm.SHA1
     
-    // The number of digits to present to the user for OTP, defaults to 6.
+    /// The number of digits to present to the user for OTP, defaults to 6.
     @Published var digits: Int = 6
     
-    // The TOTP sync period in seconds. This is the amount of time the code is valid for. Only valid when type is TOTP
+    /// The TOTP sync period in seconds. This is the amount of time the code is valid for. Only valid when type is TOTP
     var period: Int = 30
     
-    // Only if using HOTP, the user has to enter an initial counter number to sync with the server.
+    /// Only if using HOTP, the user has to enter an initial counter number to sync with the server.
     var counter: Int = 0
+    
+    /// We can use this variable as an easy way to keep the current OTP, just cycle through the list and update this
+    var currentValue: String = ""
+
+    private func generateHOTP(intervalCounter: Int) -> String {
+        let key = Base32.base32Decode(self.secret.uppercased() + String(repeating: "=", count: ((8 - self.secret.count) % 8)))
+        let counter = UInt64(intervalCounter)
+        var packedCounter = Data(count: MemoryLayout<UInt64>.size)
+        packedCounter.withUnsafeMutableBytes { (bytes: UnsafeMutablePointer<UInt64>) -> Void in
+            bytes.pointee = counter.bigEndian
+        }
+        
+        var mac = Data(HMAC<Insecure.SHA1>.authenticationCode(for: packedCounter, using: SymmetricKey(data: key!)))
+        switch self.algorithm {
+        case .SHA1:
+            mac = Data(HMAC<Insecure.SHA1>.authenticationCode(for: packedCounter, using: SymmetricKey(data: key!)))
+        case .SHA256:
+            mac = Data(HMAC<SHA256>.authenticationCode(for: packedCounter, using: SymmetricKey(data: key!)))
+        case .SHA512:
+            mac = Data(HMAC<SHA512>.authenticationCode(for: packedCounter, using: SymmetricKey(data: key!)))
+        }
+        
+        let offset = Int(mac[mac.count - 1]) & 0x0f
+        let binary = UInt32(bigEndian: mac[offset..<offset+4].withUnsafeBytes { $0.pointee }) & 0x7fffffff
+        var bString = String(binary)
+        if bString.count < self.digits {
+            bString = bString.padding(toLength: self.digits, withPad: "0", startingAt: 0)
+        }
+        return String(bString.suffix(self.digits))
+    }
+    
+    func generateCode() -> String {
+        if self.type == OTPType.TOTP {
+            return generateHOTP(intervalCounter: Int(Int(NSDate().timeIntervalSince1970) / self.period))
+        }
+        let c = self.counter
+        self.counter += 1
+        return generateHOTP(intervalCounter: c)
+    }
+    
+    func setCode() {
+        self.currentValue = generateCode()
+    }
     
     init(type: OTPType, secret: String, issuer: String, algorithm: OTPAlgorithm, digits: Int, period: Int, counter: Int) {
         self.type = type
@@ -82,6 +127,7 @@ class OTPItem: ObservableObject, Identifiable {
         self.counter = counter
     }
     
+    /// Initialize a OTP object from a OTP URL. This intitalizer can throw exceptions if the URL is malformed
     init(_ fromURL: String) throws {
         let url = URL(string: fromURL)
         
