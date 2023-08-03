24dd13a609405af31ee6d3f40dded721d6ef33d9
Tanishq Dubey <tanishq.dubey@gmail.com>
2023-08-03 08:15:30 -0400

    Start adding timestamp modification logic

--- Sesame/OTPStore.swift
+++ Sesame/OTPStore.swift
@@ -11,6 +11,7 @@ import KeychainAccess
 
 class OTPStore: ObservableObject {
     @Published var OTPs: [OTPItem] = []
+    @Published var lastTime: Int = 0
     
     
     static func load(completion: @escaping (Result<[OTPItem], Error>)->Void) {
@@ -20,6 +21,8 @@ class OTPStore: ObservableObject {
     let kChain = Keychain(service: "com.dws.Sesame.default").synchronizable(true)
 #endif
         let KEYCHAIN_APP_DATA_KEY: String = "otpstoredata"
+        let KEYCHAIN_APP_TIME_KEY: String = "otpstoremodtime"
+        
         DispatchQueue.global(qos: .background).async {
             do {
                 guard let data = try kChain.getData(KEYCHAIN_APP_DATA_KEY) else {
@@ -28,6 +31,18 @@ class OTPStore: ObservableObject {
                     }
                     return
                 }
+                var tString = "0"
+                if let tmod = try kChain.getData(KEYCHAIN_APP_TIME_KEY) {
+                    if let dataString = String(data: tmod, encoding: .utf8) {
+                        tString = dataString
+                    }
+                }
+                
+                if let tnumber = Int(tString) {
+                    let timeInterval = NSDate().timeIntervalSince1970
+                    let tData = Int(timeInterval)
+                }
+                
                 let lOTPs = try JSONDecoder().decode([OTPItem].self, from: data)
                 if (lOTPs.isEmpty) {
                     // Should only need for one build, but will keep this here.
@@ -63,11 +78,18 @@ class OTPStore: ObservableObject {
     let kChain = Keychain(service: "com.dws.Sesame.default").synchronizable(true)
 #endif
         let KEYCHAIN_APP_DATA_KEY: String = "otpstoredata"
+        let KEYCHAIN_APP_TIME_KEY: String = "otpstoremodtime"
         
         DispatchQueue.global(qos: .background).async {
             do {
                 let data = try JSONEncoder().encode(otps)
+                let timeInterval = NSDate().timeIntervalSince1970
+                let tData = String(Int(timeInterval))
+                
                 try kChain.synchronizable(true).set(data, key: KEYCHAIN_APP_DATA_KEY)
+                try kChain.synchronizable(true).set(tData, key: KEYCHAIN_APP_TIME_KEY)
+                
+                
                 DispatchQueue.main.async {
                     completion(.success(otps.count))
                 }
