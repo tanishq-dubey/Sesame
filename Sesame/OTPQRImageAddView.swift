3d5608f053046d1b1fd32d9731b08824c4d8d71a
Tanishq Dubey <tanishq.dubey@gmail.com>
2023-08-02 23:06:57 -0400

    Add capibilites to add by image, enable iCloud sync, typo fixes

---
+++ Sesame/OTPQRImageAddView.swift
@@ -0,0 +1,18 @@
+//
+//  OTPQRImageAddView.swift
+//  Sesame
+//
+//  Created by Tanishq Dubey on 7/27/23.
+//
+
+import SwiftUI
+
+struct OTPQRImageAddView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    OTPQRImageAddView()
+}
